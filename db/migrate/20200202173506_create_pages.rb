c5c932c91d17acc2fa646e5fb1fe03297bb9cb8e
tabiojisann <takumiezumi@icloud.com>
2020-02-03 17:17:42 +0900

    create homepage

---
+++ db/migrate/20200202173506_create_pages.rb
@@ -0,0 +1,10 @@
+class CreatePages < ActiveRecord::Migration[5.2]
+  def change
+    create_table :pages do |t|
+      t.text :text
+      t.string :image
+      # t.references :user, foreign_key: true
+      t.timestamps
+    end
+  end
+end
