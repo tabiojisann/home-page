8d9068e6721d5e6aa4b4415883aa6b7757cd2353
tabiojisann <takumiezumi@icloud.com>
2020-02-08 19:35:37 +0900

    create tweet

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
 
   has_many :pages
   has_many :posts
+  has_many :tweets
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_07_120507) do
+ActiveRecord::Schema.define(version: 2020_02_08_073720) do
 
   create_table "pages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "text"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 2020_02_07_120507) do
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "title"
     t.text "content"
+    t.text "image"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "text"
     t.text "image"
     t.integer "user_id"
     t.datetime "created_at", null: false
